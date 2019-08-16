a2ea057ae2fb70a9a42cbb4cec45c163007fd073
zytt <251605620@qq.com>
2019-08-16 20:36:51 +0800

    0816

--- src/views/Cinema/index.vue
+++ src/views/Cinema/index.vue
@@ -1,6 +1,23 @@
 <template>
   <div id="main">
      <Header />
+
+     <div id="content">
+			<div class="cinema_menu">
+				<div class="city_switch">
+					全城 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+				<div class="brand_swtich">
+					品牌 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+				<div class="feature_switch">
+					特色 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+			</div>
+			<Cllist />>
+		</div>
+
+
      <TabBar />
   </div>
 </template>
@@ -9,15 +26,18 @@
 
 import Header from '@/components/Header'
 import TabBar from '@/components/TabBar'
+import Cllist from '@/components/Cllist'
 export default {
     name:'cinema',
     components:{
       Header,
-      TabBar
+			TabBar,
+			Cllist
     }
 }
 </script>
 
 <style scoped>
+ #content .cinema_menu{ width: 100%; height: 45px; border-bottom:1px solid #e6e6e6; display: flex; justify-content:space-around; align-items:center; background:white;}
 
 </style>
